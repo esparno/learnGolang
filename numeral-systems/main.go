5bd68e123b43acec3b289c32ecdc9a6fe83bc719
footballfanatic\esparno <footballfanatic\esparno>
2017-06-16 19:03:20 -0600

    numeral system formating

---
+++ numeral-systems/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Decimal:", 42)
+	fmt.Printf("Binary: %d - %b \n", 42, 42) // binary
+	fmt.Printf("Hexadecimal: %d - %X \n", 42, 42) // hexadecimal
+
+}
