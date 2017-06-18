7422313f530705662c774366a24af7db41cf20fe
esparno <elsparno@gmail.com>
2017-06-17 18:20:29 -0600

    slices

---
+++ slices/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// create slice with 3 values
+	var mySlice1 []int = []int{2,7,4}
+	mySlice1 = append(mySlice1, 5,6,7)
+	fmt.Println(mySlice1)
+
+	// slice created with 5 empty values
+	mySlice2 := make([]int, 5)
+	for i:=0; i<len(mySlice2); i++ {
+		mySlice2[i] = i+2
+	}
+	fmt.Println(mySlice2)
+
+	// slice w/ 3 values
+	mySlice3 := []int{2,4,6}
+	fmt.Println(mySlice3)
+
+	// array with length 0 and capacity 10
+	mySlice4 := make([]int, 0, 10)
+	mySlice4 = append(mySlice4, 4,8,10,12,14,16)
+	fmt.Println(mySlice4)
+}
