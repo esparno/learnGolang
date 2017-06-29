ce194cbed5e7ae6e3de3dcc33edd84d45269e2ca
esparno <elsparno@gmail.com>
2017-06-28 20:42:15 -0600

    concurrency with mutexes

---
+++ concurrency/mutex/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var m sync.Mutex
+var wg sync.WaitGroup
+var count int
+func main() {
+	wg.Add(2)
+	go incrementor("Foo:")
+	go incrementor("Bar:")
+	wg.Wait()
+	fmt.Println("Final Counter:", count)
+}
+func incrementor(s string) {
+	for i:=0; i<31; i++ {
+		m.Lock()
+		count++
+		m.Unlock()
+		fmt.Println(s, i, "Counter: ", count)
+	}
+	wg.Done()
+}
