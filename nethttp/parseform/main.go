00367ed5b5fc451b020557059defcd9feaf3f475
esparno <elsparno@gmail.com>
2017-07-08 22:00:54 -0600

    http/net parseForm

---
+++ nethttp/parseform/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tpl *template.Template
+func init () {
+	tpl, _ = template.ParseFiles("index.gohtml")
+}
+func main() {
+	var Alister Dog
+	http.ListenAndServe(":8080", Alister)
+}
+type Dog struct {
+	Name string
+	Breed string
+	Age int
+}
+func (d Dog) ServeHTTP(wr http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	tpl.ExecuteTemplate(wr, "index.gohtml", req.Form)
+}
